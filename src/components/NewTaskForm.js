0b34c56fc158df1803e2c17d7b7acebc2b7e6080
benaiah-ke <benaiahpraise@gmail.com>
2022-09-26 08:59:17 +0300

    Done with lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+  const [formData, setFormData] = useState({
+    text: '',
+    category: ''
+  })
+
+  const categoryList = categories.map((category) => {
+    return <option key={category}>{category}</option>
+  })
+
+  function handleTextChange(event){
+    setFormData({
+      ...formData,
+      text: event.target.value
+    })
+  }
+
+  function handleCategoryChange(event){
+    setFormData({
+      ...formData,
+      category: event.target.value
+    })
+  }
+
+  function createTask(event){
+    event.preventDefault();
+
+    onTaskFormSubmit(formData);
+    setFormData({
+      text: '',
+      category: ''
+    })
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={createTask}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={formData.text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={formData.category} onChange={handleCategoryChange}>
+          {categoryList}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from './Task';
 
-function TaskList() {
+function TaskList({tasks, deleteTask = null}) {
+  const taskList = tasks.map((task) => {
+    return <Task key={task.text} category={task.category} text={task.text} deleteTask={deleteTask} />
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
